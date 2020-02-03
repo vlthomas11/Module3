f55d94f11e266f9632d0084fee1eb615970e415a
VLThomas11 <vthomas2@DMACC.edu>
2020-02-03 15:58:56 -0600

    adding inputs

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -1,15 +1,23 @@
-def average():
+def average(score1,score2, score3):
 
-
-    scoreA = input("Enter Score 1: ")
-    scoreB = input("Enter Score 2: ")
-    scoreC = input("Enter Score 3: ")
-
-    score1 = int(scoreA)
-    score2 = int(scoreB)
-    score3 = int(scoreC)
-    avg = average(score1 + score2 + score3)
+    scoreA = int(score1)
+    scoreB = int(score2)
+    scoreC = int(score3)
+    avg = (scoreA + scoreB + scoreC)/3
 
     return avg
 
 # pass
+
+def getting_multiple_inputs():
+    first_name = input('What is your first name? ')
+    last_name = input('What is your last name? ')
+    age = input("What is your age? ")
+    score1 = input("what is your first score? ")
+    score2 = input("What is your second score? ")
+    score3 = input("What is your thrird score? ")
+    average_scores = average(score1,score2,score3)
+    print(last_name,", ",first_name," age: ",age," average grade: ",average_scores)
+
+if __name__ == '__main__':
+    getting_multiple_inputs()
